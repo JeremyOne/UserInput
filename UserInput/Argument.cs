e949293748a247783c009785d4c4cb6e29f68a26
Jeremy Peterson <jeremy.one@gmail.com>
2016-11-19 09:42:33 -0800

    Refactor and improve, added Argument.cs

---
+++ UserInput/Argument.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeremyOne.UserInput {
+    public class Argument {
+
+        public string Name { get; set; }
+
+        private List<string> Values = new List<string>();
+
+        public Argument(string _Name, bool _Exists) {
+            Name = _Name;
+            Exists = _Exists;
+        }
+
+        public Argument(string _Name, string _Value, bool _Exists) {
+            Name = _Name;
+            Exists = _Exists;
+            Values = new List<string> { _Value };
+        }
+
+        public Argument(string _Name, string[] _Values, bool _Exists) {
+            Name = _Name;
+            Exists = _Exists;
+            Values = _Values.ToList();
+        }
+
+        public Argument this[int index] {
+            get {
+                if (index < Values.Count()) {
+                    return new Argument(Name, Values[index], true);
+                } else {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public bool Exists { get; set; }
+
+        public bool IsArray() {
+            return (Values.Count() > 1);
+        }
+
+        public bool HasValue() {
+            return (Values.Count() > 0);
+        }
+
+        public string Value {
+            get {
+                if (HasValue()) {
+                    return Values[0];
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        public string ValueOrDefault(string Default) {
+            var _value = Value;
+
+            if (_value == null) {
+                return Default;
+            } else {
+                return _value;
+            }
+        }
+
+        public int? ValueInt {
+            get {
+                int valueInt;
+
+                if (HasValue() && int.TryParse(Values[0], out valueInt)) {
+                    return valueInt;
+                } else {
+                    return null;
+                }
+            }            
+        }
+
+        public int ValueIntOrDefault(int Default) {
+            var _value = ValueInt;
+
+            if (_value == null) {
+                return Default;
+            } else {
+                return _value.Value;
+            }
+        }
+
+        public decimal? ValueDecimal {
+            get {
+                decimal valueDecimal;
+
+                if (HasValue() && decimal.TryParse(Values[0], out valueDecimal)) {
+                    return valueDecimal;
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        public decimal ValueDecimalOrDefault(decimal Default) {
+            var _value = ValueDecimal;
+
+            if (_value == null) {
+                return Default;
+            } else {
+                return _value.Value;
+            }
+        }
+
+        public long? ValueLong {
+            get {
+                long valueLong;
+
+                if (HasValue() && long.TryParse(Values[0], out valueLong)) {
+                    return valueLong;
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        public long ValueLongOrDefault(long Default) {
+            var _value = ValueLong;
+
+            if (_value == null) {
+                return Default;
+            } else {
+                return _value.Value;
+            }
+        }
+
+        public DateTime? ValueDateTime {
+            get {
+                DateTime valueDateTime;
+
+                if (HasValue() && DateTime.TryParse(Values[0], out valueDateTime)) {
+                    return valueDateTime;
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        public DateTime ValueDateTimeOrDefault(DateTime Default) {
+            var _value = ValueDateTime;
+
+            if (_value == null) {
+                return Default;
+            } else {
+                return _value.Value;
+            }
+        }
+
+        public void AddValue(string NewValue) {
+            Values.Add(NewValue);
+        }
+
+    }
+}
--- UserInput/ConsoleArguments.cs
+++ UserInput/ConsoleArguments.cs
@@ -5,276 +5,111 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace JeremyOne.UserInput {
-	public class ConsoleArguments {
+    public class ConsoleArguments : List<Argument> {
 
-		public Dictionary<string, string[]> Arguments = new Dictionary<string, string[]>();
-
-		string[] NameIndicators = { "/" };
-		char[] ValueSeperators = { ':' };
+        string[] NameIndicators = { "/" };
+        char[] ValueSeperators = { ':' };
 
         /// <param name="_Arguments">Standard arguments array from console application</param>
         /// <param name="_NameIndicators">Strings that signal the start of a argument (default: '/')</param>
         /// <param name="_ValueSeperators">Strings that signal the start of the value in an argument (default: ':')
-        /// Note: A space is ALWAYS a seperator unless values are escaped with double quotes</param>
+        /// Note: A space is ALWAYS a separator unless values are escaped with double quotes</param>
         public ConsoleArguments(string[] _Arguments, string[] _NameIndicators = null, char[] _ValueSeperators = null) {
-			if(_NameIndicators != null) {
-				//order by length to make sure that longer indicators are processed first ('--' before '-').
-				NameIndicators = _NameIndicators.OrderByDescending(a => a.Length).ToArray();
-			}
+            if (_NameIndicators != null) {
+                //order by length to make sure that longer indicators are processed first ('--' before '-').
+                NameIndicators = _NameIndicators.OrderByDescending(a => a.Length).ToArray();
+            }
 
-			if(_ValueSeperators != null) {
-				ValueSeperators = _ValueSeperators;
-			}
+            if (_ValueSeperators != null) {
+                ValueSeperators = _ValueSeperators;
+            }
 
-			ReadArguments(_Arguments);
-		}
+            ReadArguments(_Arguments);
+        }
 
         /// <param name="_Arguments">Standard arguments array from console application</param>
 		public ConsoleArguments(string[] _Arguments) {
-			ReadArguments(_Arguments);
-		}
+            ReadArguments(_Arguments);
+        }
 
         /// <summary>
         /// Parses the arguments
         /// </summary>
 		private void ReadArguments(string[] args) {
-			string lastArgName = "";
+            Argument lastArg = new Argument("", true);
+            this.Add(lastArg);
 
-			foreach(string arg in args) {
-				bool hasNameIndicator = false;
-				string nameIndicator = "";
+            foreach (string arg in args) {
+                bool hasNameIndicator = false;
+                string nameIndicator = "";
 
-				foreach(string nameStart in NameIndicators) {
-					if(arg.StartsWith(nameStart)) {
-						//the argument starts with a name indicator
-						hasNameIndicator = true;
-						nameIndicator = nameStart;
-					}
-				}
+                foreach (string nameStart in NameIndicators) {
+                    if (arg.StartsWith(nameStart)) {
+                        //the argument starts with a name indicator
+                        hasNameIndicator = true;
+                        nameIndicator = nameStart;
+                        break;
+                    }
+                }
 
-				if(hasNameIndicator == false) {
-					//argument does not start with with an indicator, so assume it is part of a list
-					AppendArgumentValue(lastArgName, arg);
-				} else {
-					string name = arg.Remove(nameIndicator.Length);
-					string[] values = { };
-					char valueSeperator = ValueSeperators[0];
+                if (hasNameIndicator == false) {
+                    //argument does not start with with an indicator, so assume it is part of a list
+                    if (arg.Contains(" ")) {
+                        lastArg.AddValue(arg.Trim('\'').Trim('"'));
+                    } else {
+                        lastArg.AddValue(arg);
+                    }
+                } else {
+                    string name = arg.Remove(0, nameIndicator.Length).ToLower();
+                    string[] values = { };
+                    char valueSeperator = ValueSeperators[0];
 
-					foreach(char seperator in ValueSeperators) {
-						if(name.Contains(seperator)) {
-							values = name.Split(seperator);
-							valueSeperator = seperator;
-						}
-					}
+                    foreach (char seperator in ValueSeperators) {
+                        if (name.Contains(seperator)) {
+                            if (name.EndsWith(seperator.ToString())) {
+                                name = name.TrimEnd(seperator);
+                            }
 
-					if(values.Length > 0) {
-						//Is an array
-						foreach(string v in values) {
-							AppendArgumentValue(values[0], v);
-						}
-					} else {
-						//Is not an array
-						AddArgument(name, null);
-					}
+                            values = name.Split(seperator);
+                            valueSeperator = seperator;
+                            name = values[0];
+                        }
+                    }
 
-				}
+                    lastArg = new Argument(name, true);
+                    this.Add(lastArg);
 
-			}
-		}
+                    if (values.Length > 1) {
+                        //Is an array
+                        for (int i = 1; i < values.Length; i++) {
+                            if (values[i].Contains(" ")) {
+                                values[i] = values[i].Trim('\'').Trim('"');
+                            }
 
-        /// <summary>
-        /// Appends a value to a argument array/list
-        /// </summary>
-		private void AppendArgumentValue(string Name, string Value) {
-			Name = Name.ToLower();
+                            lastArg.AddValue(values[i]);
+                        }
+                    }
 
-			if(ArgumentExists(Name)) {
-				var o = GetValueList(Name).ToList();
-				o.Add(Value);
-				Arguments[Name] = o.ToArray();
-			} else {
-				AddArgument(Name, Value);
-			}
-		}
+                }
 
-        /// <summary>
-        /// Adds a argument to list list of parsed arguments
-        /// </summary>
-		private void AddArgument(string Name, string Value) {
-			Name = Name.ToLower();
-
-			if(ArgumentExists(Name)) {
-				throw new ArgumentException("The argument: '" + Name + "' was already parsed");
-			} else {
-				string[] s = { Value };
-				Arguments.Add(Name, s);
-			}
-		}
+            }
+        }
 
         /// <summary>
-        /// Checks if a argument with this name exists
+        /// Gets an argument by name
         /// </summary>
-		public bool ArgumentExists(string Name) {
-			Name = Name.ToLower();
-			return (Arguments.ContainsKey(Name));
-		}
+        /// <param name="Name">Name of argument</param>
+        /// <returns>An Argument object, or if the argument does not exist a empty argument with no value is returned</returns>
+        public Argument GetArgument(string Name) {
+            Name = Name.ToLower();
+            var a = (from aa in this where aa.Name == Name select aa).FirstOrDefault();
 
-        /// <summary>
-        /// Checks if a list of arguments exists
-        /// </summary>
-        /// <returns>True if all arguments exist</returns>
-		public bool ArgumentsExists(params string[] Names) {
-			foreach(string name in Names) {
-				if(ArgumentExists(name) == false) {
-					return false;
-				}
-			}
+            if (a != null) {
+                return a;
+            } else {
+                return new Argument(Name.ToLower(), false);
+            }
+        }
 
-			return true;
-		}
-
-        /// <summary>
-        /// Checks of a argument has more than one value
-        /// </summary>
-		public bool ArgumentValueIsArray(string Name) {
-			Name = Name.ToLower();
-			if(ArgumentExists(Name)) {
-				return (Arguments[Name].Length > 1);
-			} else {
-				return false;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument
-        /// </summary>
-        /// <param name="DefaultValue">The return value if the argument is not found</param>
-        /// <returns></returns>
-		public string GetValue(string Name, string DefaultValue) {
-			Name = Name.ToLower();
-			if(ArgumentExists(Name)) {
-				return Arguments[Name.ToLower()][1];
-			} else {
-				return DefaultValue;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument
-        /// </summary>
-        public string GetValue(string Name) {
-			return GetValue(Name, null);
-		}
-
-        /// <summary>
-        /// Gets all values of a argument
-        /// </summary>
-        public string[] GetValueList(string Name) {
-			Name = Name.ToLower();
-
-			if(ArgumentExists(Name) && ArgumentValueIsArray(Name)) {
-				return Arguments[Name.ToLower()];
-			} else {
-				return null;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument as an integer
-        /// </summary>
-        /// <param name="DefaultValue">The return value if the argument is not found</param>
-        public int GetIntValue(string Name, int DefaultValue) {
-			int intValue;
-			string stringValue = GetValue(Name);
-
-			if((string.IsNullOrEmpty(stringValue) == false) &&
-			             int.TryParse(stringValue, out intValue)) {
-				return intValue;
-			} else {
-				return DefaultValue;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument as an integer
-        /// </summary>
-        /// <returns>Returns the value, or null if not found</returns>
-		public Nullable<int> GetIntValue(string Name) {
-			int intValue;
-			string stringValue = GetValue(Name);
-
-			if((string.IsNullOrEmpty(stringValue) == false) &&
-			             int.TryParse(stringValue, out intValue)) {
-				return intValue;
-			} else {
-				return null;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument as a decimal
-        /// </summary>
-        /// <param name="DefaultValue">The return value if the argument is not found</param>
-        public decimal GetDecimalValue(string Name, decimal DefaultValue) {
-			decimal decimalValue;
-			string stringValue = GetValue(Name);
-
-			if((string.IsNullOrEmpty(stringValue) == false) &&
-			             decimal.TryParse(stringValue, out decimalValue)) {
-				return decimalValue;
-			} else {
-				return DefaultValue;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument as a decimal
-        /// </summary>
-        /// <returns>Returns the value, or null if not found</returns>
-		public Nullable<decimal> GetDecimalValue(string Name) {
-			decimal decimalValue;
-			string stringValue = GetValue(Name);
-
-			if((string.IsNullOrEmpty(stringValue) == false) &&
-			             decimal.TryParse(stringValue, out decimalValue)) {
-				return decimalValue;
-			} else {
-				return null;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument as a long
-        /// </summary>
-        /// <param name="DefaultValue">The return value if the argument is not found</param>
-        public long GetLongValue(string Name, long DefaultValue) {
-			long lValue;
-			string stringValue = GetValue(Name);
-
-			if((string.IsNullOrEmpty(stringValue) == false) &&
-			             long.TryParse(stringValue, out lValue)) {
-				return lValue;
-			} else {
-				return DefaultValue;
-			}
-		}
-
-        /// <summary>
-        /// Gets the first value of a argument as a long
-        /// </summary>
-        /// <returns>Returns the value, or null if not found</returns>
-		public Nullable<long> GetLongValue(string Name) {
-			long lValue;
-			string stringValue = GetValue(Name);
-
-			if((string.IsNullOrEmpty(stringValue) == false) &&
-			             long.TryParse(stringValue, out lValue)) {
-				return lValue;
-			} else {
-				return null;
-			}
-		}
-
-	}
-    
+    }
 }

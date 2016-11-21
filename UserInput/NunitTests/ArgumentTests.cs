c7618b6ca3d9125120e3f4d5d0d4705f17e5c358
Jeremy Peterson <jeremy.one@gmail.com>
2016-11-21 10:44:07 -0800

    Adding First Version of Unit Tests

---
+++ UserInput/NunitTests/ArgumentTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JeremyOne.UserInput;
+
+namespace JeremyOne.UserInputTests {
+
+    [TestFixture]
+    public class ArgumentTests {
+
+        [Test]
+        public void ArgumentTest1() {
+            var a = new Argument("name1", "value", true);
+            var b = new Argument("name2", "99", true);
+            var c = new Argument("name3", "99.99", true);
+            var d = new Argument("name4", "123456789123", true);
+            var e = new Argument("name5", "01/01/2000", true);
+            var f = new Argument("name5", "01/01/2000 06:00:00", true);
+            string[] ga = { "value1", "value2", "3", "4.44", "555555555555", "06/06/2006" };
+            var g = new Argument("name6", ga, true);
+
+            Assert.AreEqual(a.Name, "name1", "Name is unchanged");
+            Assert.AreEqual(a.Value, "value", "Value is unchanged");
+            Assert.AreEqual(a.ValueInt, null, "Unparseable int returns null");
+            Assert.AreEqual(a.ValueDecimal, null, "Unparseable decimal returns null");
+            Assert.AreEqual(a.ValueLong, null, "Unparseable long returns null");
+            Assert.AreEqual(a.ValueDateTime, null, "Unparseable DateTime returns null");
+            Assert.AreEqual(a.ValueIntOrDefault(11), 11, "Unparseable int returns default");
+            Assert.AreEqual(a.ValueDecimalOrDefault(11.1M), 11.1M, "Unparseable Decimal returns default");
+            Assert.AreEqual(a.ValueLongOrDefault(987654321987), 987654321987, "Unparseable long returns default");
+            Assert.AreEqual(a.ValueDateTimeOrDefault(new DateTime(2000, 01, 01)), new DateTime(2000, 01, 01), "Unparseable date returns default");
+            Assert.AreEqual(a.IsArray(), false, "Array is not identified");
+
+            Assert.AreEqual(b.Value, "99", "Int readable as string");
+            Assert.AreEqual(b.ValueInt, 99, "Int readable as int");
+            Assert.AreEqual(b.ValueDecimal, 99.0, "Int readable as decimal");
+            Assert.AreEqual(b.ValueIntOrDefault(11), 99.0, "Valid int readable as with default");
+
+            Assert.AreEqual(c.ValueInt, null, "Decimal not readable as int");
+            Assert.AreEqual(c.ValueDecimal, 99.99, "Decimal readable as decimal");
+            Assert.AreEqual(c.ValueDecimalOrDefault(11.1m), 99.99, "Valid decimal readable as with default");
+
+            Assert.AreEqual(d.ValueLong, 123456789123, "Long readable as long");
+            Assert.AreEqual(d.ValueLongOrDefault(987654321987), 123456789123, "Valid long readable as with default");
+            Assert.AreEqual(d.ValueInt, null);
+
+            Assert.AreEqual(e.ValueDateTime, new DateTime(2000, 01, 01), "Date readable as date");
+            Assert.AreEqual(e.ValueDateTimeOrDefault(new DateTime(2012, 12, 12)), new DateTime(2000, 01, 01), "Valid DateTime readable as with default");
+            Assert.AreEqual(f.ValueDateTime, new DateTime(2000, 01, 01, 6, 0, 0), "Date with time readable as DateTime");
+
+            Assert.AreEqual(g.IsArray(), true, "Array argument is identified");
+            Assert.AreEqual(g[0].Value, "value1", "Array 1st value is unchanged");
+            Assert.AreEqual(g[1].Value, "value2", "Array 2nd value is unchanged");
+            Assert.AreEqual(g[2].ValueInt, 3, "Array int value is readable as int");
+            Assert.AreEqual(g[3].ValueDecimal, 4.44m, "Array decimal value is readable as decimal");
+            Assert.AreEqual(g[4].ValueLong, 555555555555, "Array long value is readable as long");
+            Assert.AreEqual(g[5].ValueDateTime, new DateTime(2006, 06, 06), "Array date value is readable as date");
+
+            Assert.Throws<IndexOutOfRangeException>(delegate {
+                var outOfRange = g[6];
+            }, "Array out of bounds throws index error");
+            Assert.Throws<ArgumentOutOfRangeException>(delegate {
+                var outOfRange = g[-1].Value;
+            }, "Array negative throws argument error");
+
+            var h = new Argument("name7", true);
+            Assert.AreEqual(h.HasValue(), false, "Argument with no value is identified");
+            Assert.AreEqual(h.IsArray(), false, "Argument with no value is identified as not array");
+            h.AddValue("1");
+            Assert.AreEqual(h.HasValue(), true, "Argument with 1 value is identified");
+            Assert.AreEqual(h.IsArray(), false, "Argument with 1 value is not identified as array");
+            h.AddValue("2");
+            Assert.AreEqual(h.IsArray(), true, "Argument with 2 values is identified as array");
+
+        }
+
+    }
+}
---
+++ UserInput/NunitTests/ConsoleArgumentsTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using NUnit.Framework;
+using JeremyOne.UserInput;
+
+namespace UserInputTests
+{
+    [TestFixture]
+    public class ConsoleArgumentsTests {
+
+        [Test]
+        public void ConsoleArgumentsTest1() {
+            string[] a1 = {
+                "value1",
+                "value2",
+                "value3",
+                "/name1:value",
+                "/name2;value",
+                "-name3:value",
+                "-name4;value",
+                "--name5:value",
+                "--name6;value",
+                "/name7:",
+                "/name8",
+                "-name9",
+                "--name10;",
+                "--name11",
+                "name11-value",
+                "/name12:",
+                "name12-value1",
+                "name12-value2",
+                "name12-value3",
+                "/name13:value1:value2:value3",
+                "/name14:\"value with spaces\"",
+                "/name15:'value with spaces'",
+                "/name16",
+                "'value with spaces'",
+                "\"value with spaces\""
+            };
+
+            string[] names = { "/", "-", "--" };
+            char[] seperators = { ':', ';'};
+
+            var c1 = new ConsoleArguments(a1);
+            var c2 = new ConsoleArguments(a1, names, seperators);
+
+            Assert.AreEqual(c1.GetArgument("").HasValue(), true, "Unnamed argument has value");
+            Assert.AreEqual(c1.GetArgument("").Exists, true, "Unnamed argument exists");
+            Assert.AreEqual(c1.GetArgument("").IsArray(), true, "Unnamed argument flagged as array");
+            Assert.AreEqual(c1.GetArgument("")[0].Value, "value1", "Unnamed argument accessed with indexer has value");
+            Assert.AreEqual(c1.GetArgument("")[1].Value, "value2", "Unnamed argument accessed with indexer has value");
+            Assert.AreEqual(c1.GetArgument("")[2].Value, "value3", "Unnamed argument accessed with indexer has value");
+
+            Assert.AreEqual(c1.GetArgument("name1").HasValue(), true, "Existing argument has value");
+            Assert.AreEqual(c1.GetArgument("name1").Exists, true, "Existing argument flagged correctly");
+            Assert.AreEqual(c1.GetArgument("name1").Value, "value", "Value is unchanged");
+            Assert.AreEqual(c1.GetArgument("name3").Value, null, "Incorrect name indicator returns null");
+
+            Assert.AreEqual(c1.GetArgument("1eman").HasValue(), false, "Non-existent argument does not have value");
+            Assert.AreEqual(c1.GetArgument("1eman").Exists, false, "Non-existent argument flagged correctly");
+
+            Assert.AreEqual(c2.GetArgument("name3").Value, "value", "Correct name indicator returns value");
+            Assert.AreEqual(c2.GetArgument("name4").Value, "value", "Correct name indicator returns value");
+            Assert.AreEqual(c2.GetArgument("name5").Value, "value", "Correct name indicator returns value");
+            Assert.AreEqual(c2.GetArgument("name6").Value, "value", "Correct name indicator returns value");
+
+            Assert.AreEqual(c2.GetArgument("name7").Exists, true, "Correct name indicator (no value) exists");
+            Assert.AreEqual(c2.GetArgument("name8").Exists, true, "Correct name indicator (no value) exists");
+            Assert.AreEqual(c2.GetArgument("name9").Exists, true, "Correct name indicator (no value) exists");
+            Assert.AreEqual(c2.GetArgument("name10").Exists, true, "Correct name indicator (no value) exists");
+
+            Assert.AreEqual(c2.GetArgument("name11").Value, "name11-value", "Multi parameter argument read correctly");
+
+            Assert.AreEqual(c2.GetArgument("name12").Value, "name12-value1", "Multi pram array argument read correctly");
+            Assert.AreEqual(c2.GetArgument("name12").IsArray(), true, "Multi pram array argument identified correctly");
+            Assert.AreEqual(c2.GetArgument("name12")[0].Value, "name12-value1", "Multi pram array argument access with indexer correct");
+            Assert.AreEqual(c2.GetArgument("name12")[1].Value, "name12-value2", "Multi pram array argument access with indexer correct");
+            Assert.AreEqual(c2.GetArgument("name12")[2].Value, "name12-value3", "Multi pram array argument access with indexer correct");
+
+            Assert.AreEqual(c2.GetArgument("name13").Value, "value1", "Multi array argument read correctly");
+            Assert.AreEqual(c2.GetArgument("name13").IsArray(), true, "Multi array argument identified correctly");
+            Assert.AreEqual(c2.GetArgument("name13")[0].Value, "value1", "Multi array argument access with indexer correct");
+            Assert.AreEqual(c2.GetArgument("name13")[1].Value, "value2", "Multi array argument access with indexer correct");
+            Assert.AreEqual(c2.GetArgument("name13")[2].Value, "value3", "Multi array argument access with indexer correct");
+
+            Assert.AreEqual(c2.GetArgument("name14").Value, "value with spaces", "Value with spaces read correctly");
+            Assert.AreEqual(c2.GetArgument("name15").Value, "value with spaces", "Value with spaces read correctly, single quote");
+            Assert.AreEqual(c2.GetArgument("name16").Value, "value with spaces", "Value with spaces read correctly, multi parameter");
+            Assert.AreEqual(c2.GetArgument("name16")[1].Value, "value with spaces", "Value with spaces in array read correctly, multi parameter");
+        }
+    }
+}
